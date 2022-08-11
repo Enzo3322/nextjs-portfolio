1d5c2e586fe1c297c9b9c4cf86bd7f72eee20b37
Enzo3322 <enzo.spag14@gmail.com>
2022-08-10 22:29:53 -0300

    link

--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,13 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<a
+				<link
+					rel="stylesheet"
 					href="https://fonts.googleapis.com/css?family=DM+Sans:regular,italic,500,500italic,700,700italic"
-					rel="stylesheet"
-					as="font"
 				/>
-				<a
-					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+				<link
 					rel="stylesheet"
-					as="font"
+					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
 				/>
 			</Head>
 			<body>
