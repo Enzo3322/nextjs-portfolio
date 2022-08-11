2ca5c44be77c361518c0f6e94284ba17af67ad38
Enzo3322 <enzo.spag14@gmail.com>
2022-08-10 21:54:18 -0300

    fix import

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,13 @@ export default function Document() {
 			<Head>
 				<link
 					href="https://fonts.googleapis.com/css?family=DM+Sans:regular,italic,500,500italic,700,700italic&display=optional"
-					rel="stylesheet"
+					rel="preload"
+					as="font"
 				/>
 				<link
 					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&display=optional"
-					rel="stylesheet"
+					rel="preload"
+					as="font"
 				/>
 			</Head>
 			<body>
