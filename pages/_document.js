4ed2cb6c894d537844b767d265fb0466dbfef06b
Enzo3322 <enzo.spag14@gmail.com>
2022-08-10 21:59:15 -0300

    Link better than link

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<link
+				<Link
 					href="https://fonts.googleapis.com/css?family=DM+Sans:regular,italic,500,500italic,700,700italic"
 					rel="preload"
 					as="font"
 				/>
-				<link
+				<Link
 					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
 					rel="preload"
 					as="font"
