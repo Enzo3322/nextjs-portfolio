b0d719468ec2c67521f4df11dcc2be2bb0e82704
Enzo3322 <enzo.spag14@gmail.com>
2022-08-10 21:57:59 -0300

    rel

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,12 @@ export default function Document() {
 		<Html>
 			<Head>
 				<link
-					href="https://fonts.googleapis.com/css?family=DM+Sans:regular,italic,500,500italic,700,700italic&display=optional"
+					href="https://fonts.googleapis.com/css?family=DM+Sans:regular,italic,500,500italic,700,700italic"
 					rel="preload"
 					as="font"
 				/>
 				<link
-					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&display=optional"
+					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
 					rel="preload"
 					as="font"
 				/>
