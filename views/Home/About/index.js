451367ba3f2575ef3763f909d55bb87a7c136030
Enzo3322 <enzo.spag14@gmail.com>
2022-12-11 13:48:15 -0300

    use intersection observer to trigger animation

--- views/Home/About/index.js
+++ views/Home/About/index.js
@@ -1,42 +1,59 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { ArrowRight } from "../../../assets/icons";
-import styles from "../../../styles/home-about.module.scss";
-import Link from "next/link";
-import { ToolsComponent } from "../../../components/Tolls";
-import { TollsArr } from "../../../mock";
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { ArrowRight } from '../../../assets/icons';
+import styles from '../../../styles/home-about.module.scss';
+import { useInView } from 'react-intersection-observer';
+import Link from 'next/link';
 
 export const About = () => {
-  return (
-    <section className={styles.about}>
-      <motion.div whileHover={{ scale: 1.02 }} className={styles.container}>
-        <div className={styles.left}>
-          <picture>
-            <img src="/me.svg" alt="Enzo Spagnolli" />
-          </picture>
-        </div>
-        <div className={styles.right}>
-          <div>
-            <h2>About me</h2>
-            <p>
-              I've been a Web developer for over 3 years focusing on developing
-              solutions for medium and large E-commerces, I have experience in
-              developing NodeJs microservices and automated testing with
-              Cypress.io.
-            </p>
-            <p>
-              I have a degree in Systems Analysis and Development and I am
-              currently a Senior Frontend Developer.
-            </p>
-          </div>
+	const control = useAnimation();
+	const [ref, inView] = useInView();
+	const boxVariant = {
+		visible: { opacity: 1, scale: 1, transition: { duration: 1 }, left: 0 },
+		hidden: { opacity: 0 },
+	};
+	useEffect(() => {
+		if (inView) {
+			control.start('visible');
+		}
+	}, [control, inView]);
 
-          <Link href="/about/">
-            <div className={styles.link}>
-              See more <ArrowRight />
-            </div>
-          </Link>
-        </div>
-      </motion.div>
-    </section>
-  );
+	return (
+		<section className={styles.about}>
+			<motion.div
+				ref={ref}
+				variants={boxVariant}
+				animate={control}
+				initial="hidden"
+				className={styles.container}
+			>
+				<div className={styles.left}>
+					<picture>
+						<img src="/me.svg" alt="Enzo Spagnolli" />
+					</picture>
+				</div>
+				<div className={styles.right}>
+					<div>
+						<h2>About me</h2>
+						<p>
+							I've been a Web developer for over 3 years focusing on developing
+							solutions for medium and large E-commerces, I have experience in
+							developing NodeJs microservices and automated testing with
+							Cypress.io.
+						</p>
+						<p>
+							I have a degree in Systems Analysis and Development and I am
+							currently a Senior Frontend Developer.
+						</p>
+					</div>
+
+					<Link href="/about/">
+						<div className={styles.link}>
+							See more <ArrowRight />
+						</div>
+					</Link>
+				</div>
+			</motion.div>
+		</section>
+	);
 };
