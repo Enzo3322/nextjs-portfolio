349e6f006ce5a06ca83abc0dd6f22643729a2b1a
Enzo3322 <enzo.spag14@gmail.com>
2022-12-28 11:18:11 -0300

    feat: implement clarity

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
-import Head from 'next/head';
-import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
-import '../styles/globals.scss';
+import Head from "next/head";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import "../styles/globals.scss";
 function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Head>
-				<title>Enzo Spagnolli</title>
-			</Head>
-			<Header />
-			<Component {...pageProps} />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Enzo Spagnolli</title>
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-	return (
-		<Html>
-			<Head>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css?family=DM+Sans:regular,italic,500,500italic,700,700italic"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
-				/>
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=DM+Sans:regular,italic,500,500italic,700,700italic"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+        />
+        <script type="text/javascript"></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "f5gwcpmu2a");`,
+          }}
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
